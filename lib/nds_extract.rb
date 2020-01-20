e2551b70f21a36ef1769d9fc260aee4792bd6167
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-20 09:17:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,9 +24,7 @@ def directors_totals(nds)
   directors_index = 0
   while directors_index < directors_database.length do
     movies_index = 0
-    result = {
-      directors_database[directors_index][:name] => 0
-    }
+    result[directors_database[directors_index][:name]] = 0
     while movies_index < directors_database[directors_index][:movies].length do
       result[directors_database[directors_index][:name]] += directors_database[directors_index][:movies][movies_index][:worldwide_gross]
       movies_index += 1
